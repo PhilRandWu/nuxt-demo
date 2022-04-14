09e256704738530203f5495301b36499b2dc5ea3
PhilRandWu <3378814052@qq.com>
2022-04-14 13:41:57 +0800

    服务器配置

---
+++ layouts/error.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: PhilRandWu
+ * @Github: https://github/PhilRandWu
+ * @Date: 2022-04-14 13:30:15
+ * @LastEditTime: 2022-04-14 13:31:27
+ * @LastEditors: PhilRandWu
+-->
+<template>
+  <div>
+    <div>url: {{ error.path }}</div>
+    <div>code: {{ error.statusCode }}</div>
+    <div>msg: {{ error.message }}</div>
+    <nuxt-link to="/home">返回首页</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["error"],
+};
+</script>
+
+<style>
+</style>
---
+++ server/index.js
@@ -0,0 +1,39 @@
+/*
+ * @Description: 
+ * @Author: PhilRandWu
+ * @Github: https://github/PhilRandWu
+ * @Date: 2022-04-14 13:32:59
+ * @LastEditTime: 2022-04-14 13:39:29
+ * @LastEditors: PhilRandWu
+ */
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/login', async (req, res) => {
+    try {
+        if (req.body.user === 'root' && req.body.pass === 'root') {
+            res.cookie('user', '小丁');
+            res.cookie('session', '123');
+            res.json({
+                name: '小丁',
+                msg: '登录成功',
+                code: 200,
+            });
+        } else {
+            res.json({
+                code: 400,
+                msg: '密码或者用户名错误'
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        res.statusCode(500).send('服务器错误');
+    }
+})
+
+app.listen('5000',() => {
+    console.log('服务器启动成功');
+})
